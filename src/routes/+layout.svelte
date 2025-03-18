d3aa7626c02837fac9aa0472066a2660bf9452c6
MarKr17 <mariakroczek199@gmail.com>
2025-03-18 18:25:28 +0100

    first steps, colors adjusted

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	function toggle() {
+    	window.document.body.classList.toggle('dark-mode')
+	}
 </script>
+<ul id="menu">
+	<li><a href="/">Home</a>|
+	<li><a href="/Projects">Projects</a>|
+	<li><button on:click={toggle}>Toggle mode</button></li>
+</ul>
+
 
 {@render children()}
