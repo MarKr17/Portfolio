8cf493a106789f0f53e7f986657b2e572d52324b
MarKr17 <mariakroczek199@gmail.com>
2025-03-19 08:14:44 +0100

    fxed issues with github routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,11 @@
 	function toggle() {
     	window.document.body.classList.toggle('dark-mode')
 	}
+	import { base } from '$app/paths';
 </script>
 <ul id="menu">
-	<li><a href="/">Home</a>|
-	<li><a href="/Projects">Projects</a>|
+	<li><a href="{base}/">Home</a>|
+	<li><a href="{base}/Projects">Projects</a>|
 	<li><button on:click={toggle}>Toggle mode</button></li>
 </ul>
 
