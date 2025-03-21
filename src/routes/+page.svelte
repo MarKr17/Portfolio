b10f9259262cf6f63aa12830e46fb2253b5fd4ae
MarKr17 <mariakroczek199@gmail.com>
2025-03-21 10:45:35 +0100

    about layout prepered

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
     import * as json from '$lib/data/about.json'
 </script>
-<div class="flex flex-col min-w-screen justify-center items-center">
-    
-    <h1>Name Surname</h1>
-    <img src="/assets/Avatar.png" alt="My avatar"/>
-    <div class="flex flex-col min-w-screen justify-center items-center">
-        <p class="text-center">{json.text}</p>
+<div class="flex flex-col min-w-screen justify-center items-center content-between gap-10">
+    <div>
+        <h1>Name Surname</h1>
+    </div>
+    <div class=" w-50 sm:w-xs">
+        <img class="rounded-full w-2% h-2%" src="/assets/Avatar.png" alt="My avatar"/>
+    </div>
+    <div class="w-xs sm:w-lg text-center">
+        <p>{json.text}</p>
     </div>
     
 </div>
--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import * as json from '$lib/data/projects.json'
 </script>
-<h1>This is a project page</h1>
+<div class="flex flex-col min-w-screen justify-center items-center content-between gap-10">
+    <h1>This is a project page</h1>
+</div>
+
 <p> {json.Project1.description} </p>
-<img src={json.Project1.image} alt = "Project 1"/>
+<img class="h-auto max-w-lg rounded-xl" src={json.Project1.image} alt = "Project 1"/>
