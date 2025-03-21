645c94e6459c10971bbf914ce42e34d378035136
MarKr17 <mariakroczek199@gmail.com>
2025-03-21 15:53:33 +0100

    updates

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import * as json from '$lib/data/projects.json'
+    import projects from '$lib/data/projects.json'
     import CategoriesButtons from './Categories_buttons.svelte';
     let Value;
 	
@@ -14,13 +14,25 @@
 		label: 'Bioinformatics',
 	}]
 </script>
-<div class="flex flex-col min-w-screen justify-center items-center content-between gap-10">
+<div class="flex flex-col min-w-screen justify-center items-center content-between gap-2 pb-10">
     <h2>Projects</h2>
     <div class="flex justify-center">
     <CategoriesButtons {options} legend='' bind:userSelected={Value}/>
-
     </div>
-    <p> {json.Project1.description} </p>
-<img class="h-auto max-w-lg rounded-xl" src={json.Project1.image} alt = "Project 1"/>
 </div>
-
+<div class="flex flex-col min-w-screen items-center  h-150 ">
+    <div class="overflow-auto border-2 rounded-xl pr-20 pl-10">
+    {#each Object.values(projects) as project}
+    <div class="pt-5 pb-5">
+        <div class="flex flex-cols-2 gap-5">
+            <img class="h-40 max-w-xs rounded-xl" src={project.image} />
+            <div>
+                <strong>{project.name}</strong>
+                <p>{project.description}</p>
+            </div>
+        </div>
+    </div>
+    {/each}
+    <!-- ... -->
+    </div>
+</div>
