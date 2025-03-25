f1620d86842029a1a517cd54d011f18273c2cd82
MarKr17 <mariakroczek199@gmail.com>
2025-03-25 14:37:38 +0100

    changed colors, filtering now works

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -4,15 +4,28 @@
     let Value;
 	
 		const options = [{
-		value: 'green',
+		value: 'data-analytics',
 		label: 'Data Analytics',
 	}, {
-		value: 'red',
+		value: 'machine-learning',
 		label: 'Machine Learning',
 	}, {
-		value: 'blue',
+		value: 'bioinformatics',
 		label: 'Bioinformatics',
 	}]
+
+    $: filtered_Projects = Object.fromEntries(
+        Object.entries(projects).filter(([id, project]) =>{
+        console.log("---")
+        console.log(projects);
+        console.log(filtered_Projects);
+        console.log(Value);
+        console.log("---")
+        if (Value === undefined || Value.length === 0) {
+            return project;
+        }
+       return project.categories.includes(Value);
+    }));
 </script>
 <div class="flex flex-col min-w-screen justify-center items-center content-between gap-2 pb-10">
     <h2>Projects</h2>
@@ -22,7 +35,7 @@
 </div>
 <div class="flex flex-col min-w-screen items-center  h-150">
     <div class="overflow-auto border-2 rounded-xl pr-20 pl-10 ">
-    {#each Object.values(projects) as project}
+    {#each Object.values(filtered_Projects) as project}
     <div class="pt-5 pb-5">
         <div class="flex flex-cols-2 gap-5">
             <img class="h-40 max-w-xs rounded-xl" src={project.image} alt="Project image"/>
